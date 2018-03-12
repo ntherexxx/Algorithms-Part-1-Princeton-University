486e672d9ce0d7c4fceced32d2e9a1ec93a00bbc
ntherexxx <franklolxu@gmail.com>
2018-03-12 02:01:39 -0400

    Modify

--- Week 1 - Union-Find/Percolation.java
+++ Week 1 - Union-Find/Percolation.java
@@ -2,36 +2,51 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-package edu.princeton.cs.algs4;
-
 public class Percolation {
-public Percolation(int n)
-// create n-by-n grid, with all sites blocked
+  private WeightedQuickUnionUF grid;
+
+  public Percolation(int n){
 
 
-public void open(int row, int col)
-// open site (row, col) if it is not open already
+    System.out.println("Creating Grid...");
+  }
+  // create n-by-n grid, with all sites blocked
+
+  public void open(int row, int col){
+    int row_num = row - 1;
+    int col_num = col - 1;
+    grid[row_num][col_num] = true;
+
+    System.out.println("Opening " + row + ' ' + col);
+    //Open 3 2
+    //In fact open grid[2][1]
+  }
+  // open site (row, col) if it is not open already
+
+  public boolean isOpen(int row, int col){
+    return grid[row-1][col-1];
+  }
+  // is site (row, col) open?
 
 
-public boolean isOpen(int row, int col)
-// is site (row, col) open?
+  public boolean isFull(int row, int col){
+    //Full: connected to top row
+
+  }
+  // is site (row, col) full?
 
 
-public boolean isFull(int row, int col)
-// is site (row, col) full?
+  public int numberOfOpenSites()
+  // number of open sites
 
 
-public int numberOfOpenSites()
-// number of open sites
-
-
-public boolean percolates()
-// does the system percolate?
+  public boolean percolates()
+  // does the system percolate?
 
 
 
-public static void main(String[] args)
-// test client (optional)
+  public static void main(String[] args)
+  // test client (optional)
 
 
 }
