8a6adf602fae2672ed56c42513924b0ff1db8b48
ntherexxx <franklolxu@gmail.com>
2018-03-20 22:49:46 -0400

    update 4

--- Week 2 - Stack and Queue/RandomizedQueue.java
+++ Week 2 - Stack and Queue/RandomizedQueue.java
@@ -11,15 +11,15 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         n = 0;
         capacity = 1;
         s = (Item[]) new Object[capacity];
-    }                 
+    }
 // construct an empty randomized queue
     public boolean isEmpty() {
         return n == 0;
-    }                
+    }
     // is the randomized queue empty?
     public int size() {
         return n;
-    }              
+    }
     // return the number of items on the randomized queue
     public void enqueue(Item item) {
         if (item == null)
@@ -38,13 +38,13 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             }
             s = tmp;
         }
-    }          
+    }
     // add the item
     public Item dequeue() {
         if (isEmpty()) {
             throw new NoSuchElementException("dequeue has empty array");
         }
-        
+
         int index = StdRandom.uniform(n);
         Item tmpItem = s[index];
         while (tmpItem == null) {
@@ -52,20 +52,21 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             tmpItem = s[index];
         }
         n--;
-        if (n <= (capacity / 4)) {
+        if (n < (capacity / 4)) {
             capacity /= 2;
             Item[] tmp = (Item[]) new Object[capacity];
             index = 0;
-            for (Item i : s) {
-                if (i != null) {
-                    tmp[index++] = i;
+            for (int i = 0; i < n; i++) {
+                if (s[i] != null) {
+                    tmp[index] = s[i];
+                    index++;
                 }
             }
             s = tmp;
         }
-        
+
         return tmpItem;
-    }                   
+    }
     // remove and return a random item
     public Item sample() {
         if (isEmpty()) {
@@ -78,24 +79,24 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             tmpItem = s[index];
         }
         return tmpItem;
-    }                    
+    }
     // return a random item (but do not remove it)
     public Iterator<Item> iterator() {
         return new ListIterator();
-    }        
+    }
     // return an independent iterator over items in random order
     private class ListIterator implements Iterator<Item> {
         public boolean hasNext() {
             return n > 0;
         }
-        
+
         public void remove() {
             throw new UnsupportedOperationException("remove is not supported");
         }
-        
+
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException("next has no element");
-                
+
             int index = StdRandom.uniform(n);
             Item tmpItem = s[index];
             while (tmpItem == null) {
