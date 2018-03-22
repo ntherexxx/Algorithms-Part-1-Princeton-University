e31266af01cd113fdf409645cd0939c441054b9d
ntherexxx <franklolxu@gmail.com>
2018-03-22 12:23:05 -0400

    Final Update

--- Week 1 - Union-Find/PercolationStats.java
+++ Week 1 - Union-Find/PercolationStats.java
@@ -6,6 +6,9 @@ public class PercolationStats {
     private final double[] testStat;
     private static final double FACTOR = 1.96;
 
+    private double mean;
+    private double stddev;
+
     public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
     {
         if (n <= 0) throw new IllegalArgumentException("n must be greated than 0.");
@@ -29,27 +32,33 @@ public class PercolationStats {
 
             testStat[i] = (double) siteCount/(n * n);
         }
+
+        mean = StdStats.mean(testStat);
+        stddev = StdStats.stddev(testStat);
     }
     public double mean() {
-        return StdStats.mean(testStat);
+        return mean;
     }                          // sample mean of percolation threshold
     public double stddev() {
-        return StdStats.stddev(testStat);
+        return stddev;
     }                        // sample standard deviation of percolation threshold
     public double confidenceLo() {
-        return mean() - ((FACTOR * stddev()) / Math.sqrt(numTest));
+        return mean - ((FACTOR * stddev / Math.sqrt(numTest)));
     }                  // low  endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + ((FACTOR * stddev()) / Math.sqrt(numTest));
+        return mean + ((FACTOR * stddev / Math.sqrt(numTest)));
     }                  // high endpoint of 95% confidence interval
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
         PercolationStats ps = new PercolationStats(n, trials);
-
-        System.out.println("mean of p :"+ ps.mean());
-        System.out.println("standard deviation :"+ ps.stddev());
+        
+        double mean = ps.mean();
+        double stddev = ps.stddev();
+        
+        System.out.println("mean of p :"+ mean);
+        System.out.println("standard deviation :"+ stddev);
         System.out.println("confidence interval low :"+ ps.confidenceLo());
         System.out.println("confidence interval high :"+ ps.confidenceHi());
     }        // test client (described below)
