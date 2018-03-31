7ebd86193494e57bd9f022361b51cbbc4ad46fc3
ntherexxx <franklolxu@gmail.com>
2018-03-30 23:53:22 -0400

    update 3

--- Week 3 - MergeSort/BruteCollinearPoints.java
+++ Week 3 - MergeSort/BruteCollinearPoints.java
@@ -2,30 +2,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BruteCollinearPoints {
-    private ArrayList<LineSegment> foundSegments = new ArrayList<LineSegment>();
-
+    private final ArrayList<LineSegment> foundSegments = new ArrayList<LineSegment>();
+    
     public BruteCollinearPoints(Point[] points) {
         int len = points.length;
         if (len == 0) throw new IllegalArgumentException("No elements");
         // Check duplicates
         for (int i = 0; i < len - 1; i++) {
             for (int j = i + 1; j < len; j++) {
+                if (points[i] == null) throw new IllegalArgumentException("Null Element");
                 if (points[i].compareTo(points[j]) == 0) throw new IllegalArgumentException("Duplicate Elements");
             }
         }
-
+        
         Point[] pointsCopy = points.clone();
-
+        
         Arrays.sort(pointsCopy);
-
+        
         len = pointsCopy.length;
         // p q r s
         for (int p = 0; p < len - 3; p++) {
             for (int q = p + 1; q < len - 2; q++) {
                 for (int r = q + 1; r < len - 1; r++) {
                     for (int s = r + 1; s < len; s++) {
-                        if(pointsCopy[p].slopeTo(pointsCopy[q]) == pointsCopy[p].slopeTo(pointsCopy[r]) &&
-                           pointsCopy[p].slopeTo(pointsCopy[r]) == pointsCopy[p].slopeTo(pointsCopy[s])) {
+                        if (pointsCopy[p].slopeTo(pointsCopy[q]) == pointsCopy[p].slopeTo(pointsCopy[r]) &&
+                            pointsCopy[p].slopeTo(pointsCopy[r]) == pointsCopy[p].slopeTo(pointsCopy[s])) {
                             foundSegments.add(new LineSegment(pointsCopy[p], pointsCopy[s]));
                         }
                     }
--- Week 3 - MergeSort/FastCollinearPoints.java
+++ Week 3 - MergeSort/FastCollinearPoints.java
@@ -2,26 +2,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FastCollinearPoints {
-    private ArrayList<LineSegment> foundSegments = new ArrayList<LineSegment>();
-    
+    private final ArrayList<LineSegment> foundSegments = new ArrayList<LineSegment>();
+
     public FastCollinearPoints(Point[] points) {
         int len = points.length;
-        
+
         if (len == 0) throw new IllegalArgumentException("No elements");
         // Check duplicates
         for (int i = 0; i < len - 1; i++) {
             for (int j = i + 1; j < len; j++) {
+                if (points[i] == null) throw new IllegalArgumentException("Null Element");
                 if (points[i].compareTo(points[j]) == 0) throw new IllegalArgumentException("Duplicate Elements");
             }
         }
-        
+
         Point[] pointsCopy = points.clone();
         len = pointsCopy.length;
-        
+
         for (int i = 0; i < len - 3; i++) {
             Arrays.sort(pointsCopy);
-            Arrays.sort(pointsCopy, pointsCopy[i].slopeOrder());;
-            
+            Arrays.sort(pointsCopy, pointsCopy[i].slopeOrder());
+
             for (int p = 0, next = 1, last = 2; last < len; last++) {
                 while (last < len && pointsCopy[p].slopeTo(pointsCopy[next]) == pointsCopy[p].slopeTo(pointsCopy[last])) {
                     last++;
@@ -29,7 +30,7 @@ public class FastCollinearPoints {
                 if (last - next >= 3 && pointsCopy[p].compareTo(pointsCopy[last]) < 0) {
                     foundSegments.add(new LineSegment(pointsCopy[p], pointsCopy[last - 1]));
                 }
-                
+
                 next = last;
             }
         }
